423db4fe6c2c6235298acf9b509ec895dc80fbf4
Jason Pickering <jason.p.pickering@gmail.com>
2024-12-17 10:53:47 +0100

    More updates

--- metadataMonitor.py
+++ metadataMonitor.py
@@ -3,7 +3,10 @@ import time
 from urllib.parse import urlencode
 
 import urllib3
+import random
+import string
 import configparser
+import argparse
 import base64
 import logging
 
@@ -26,17 +29,25 @@ def get_integrity_summary_from_code(code, summaries):
             return summaries[key]
     return None
 
+def generate_uid():
+    first_letter = random.choice(string.ascii_lowercase)
+    last_part = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    return first_letter + last_part
+
+
 
 class MetadataMonitor:
-    def __init__(self):
+    def __init__(self,config_path):
         self.config = configparser.ConfigParser()
-        self.config.read("config.ini")
+        self.config.read(config_path)
         self.metadata_url = self.config.get("server", "server_url")
         self.metadata_username = self.config.get("server", "server_username", fallback=None)
         self.metadata_password = self.config.get("server", "server_password", fallback=None)
         self.metadata_token = self.config.get("server", "server_token", fallback=None)
         self.default_coc = self.config.get("server", "default_coc",fallback="HllvX50cXC0")
+        self.default_cc = self.config.get("server", "default_cc", fallback="bjDvmb4bfuf")
         self.logging_level = self.config.get("server", "logging_level", fallback="INFO")
+        self.log_file = self.config.get("server", "log_file", fallback="metadata_monitor.log")
         self.aggregate_dataset = self.config.get("server", "aggregate_dataset")
         self.monitoring_group = self.config.get("server", "monitor_data_element_group")
 
@@ -69,7 +80,7 @@ class MetadataMonitor:
         self.metadata = None
 
         # init the log if needed
-        logging.basicConfig(filename='metadata_monitor.log', level=self.logging_level,
+        logging.basicConfig(filename=self.log_file, level=self.logging_level,
                             format='%(asctime)s:%(levelname)s:%(message)s')
 
     def get_metadata_integrity_checks(self):
@@ -95,6 +106,68 @@ class MetadataMonitor:
         except Exception as e:
             logging.error("Error: " + str(e))
             return None
+
+    def get_integrity_checks_no_data_elements(self):
+        checks = self.get_metadata_integrity_checks()
+        #Exclude any slow or programmatic checks
+        checks = [check for check in checks if not check["isSlow"] and not check["isProgrammatic"]]
+        des = self.get_dataelements_in_dataset()
+        #Remove the MI_ prefix from each code
+        for de in des["dataSetElements"]:
+            this_code = de["dataElement"]["code"][3:]
+            for check in checks:
+                if check["code"] == this_code:
+                    checks.remove(check)
+        return checks
+
+    def create_data_element_from_integrity_check(self,check):
+        uid = generate_uid()
+        data = {
+            "id": uid,
+            "name": "[MI] " + check.get("displayName", "Placeholder metadata integrity check " + uid),
+            "shortName": check["name"].replace("_", " ").title()[:50],
+            "aggregationType": "AVERAGE",
+            "valueType": "INTEGER_ZERO_OR_POSITIVE",
+            "domainType": "AGGREGATE",
+            "code": "MI_" + check["code"],
+            "categoryCombo": {"id": self.default_cc}
+        }
+        return data
+
+    def create_missing_data_elements(self):
+        checks = self.get_integrity_checks_no_data_elements()
+        if not checks:
+            logging.info("No missing data elements to create")
+            return
+
+        for check in checks:
+            #Check to see if the data elements exists
+            response = self.http.request("GET", self.metadata_url + "/api/dataElements?filter=code:eq:" + "MI_" + check["code"],
+                                            headers=self.metadata_headers)
+            if response.status == 200:
+                logging.info("Data element already exists: " + check["code"] + " but may not be in dataset. Skipping creation.")
+                continue
+
+            logging.info("Creating new  data element for check" + check["code"])
+            data_element = self.create_data_element_from_integrity_check(check)
+            response = self.http.request("POST", self.metadata_url + "/api/dataElements",
+                                         headers=self.metadata_headers,
+                                         body=json.dumps(data_element))
+            if response.status == 201:
+                logging.info("Successfully created new data element: " + data_element["name"])
+            else:
+                logging.error("Failed to create data element: " + data_element["name"] + " with response: " + response.data.decode("utf-8"))
+                continue
+
+            response_ds =self.http.request("POST", self.metadata_url + "/api/dataSets/" + self.aggregate_dataset + "/dataElements",
+                                            headers=self.metadata_headers,
+                                            body=json.dumps({"additions" : [{"id": data_element["id"]}]}))
+            if response_ds.status == 200:
+                logging.info("Adding data element to dataset: " + data_element["name"])
+            else:
+                logging.error("Failed to add data element to dataset: " + data_element["name"] + " with response: " + response_ds.data.decode("utf-8"))
+
+
 
     def trigger_metadata_integrity_summaries(self):
         # POST /api/dataIntegrity/summary
@@ -179,7 +252,6 @@ class MetadataMonitor:
             return None
 
     def get_dataelements_in_dataset(self):
-        # GET dataSets/ySAQjSSyLQg?fields=dataSetElements[dataElement[id,code]]
         try:
             response = self.http.request("GET",
                                          self.metadata_url + "/api/dataSets/" + self.aggregate_dataset + "?fields=dataSetElements[dataElement[id,code]]",
@@ -194,9 +266,7 @@ class MetadataMonitor:
         try:
             response = self.http.request("GET", self.metadata_url + "/api/organisationUnits?level=1",
                                          headers=self.metadata_headers)
-            logging.info(response.data.decode("utf-8"))
-            logging.info(response.status)
-            logging.info("Level 1 orgunits: " + str(json.loads(response.data.decode("utf-8"))))
+            logging.debug("Level 1 orgunits: " + str(json.loads(response.data.decode("utf-8"))))
             return json.loads(response.data.decode("utf-8"))
         except Exception as e:
             logging.error("Error: " + str(e))
@@ -219,14 +289,21 @@ class MetadataMonitor:
         if failed_checks:
             logging.info(f"Failed to process data values for: {failed_checks}")
 
+    def update_metadata_integrity_data_elements(self):
+        logging.info("Starting metadata monitor")
+        checks = self.get_metadata_integrity_checks()
+        summaries = self.get_all_metadata_integrity_summaries()
+        des = self.get_data_elements_to_monitor()
+        orgunit = self.get_level1_orgunits()
+        period = time.strftime("%Y%m%d")
+        self.process_completed_checks_to_data_values(summaries, period, orgunit["organisationUnits"][0]["id"], des)
+        logging.info("Metadata monitor completed")
+
 
 if __name__ == '__main__':
-    logging.info("Starting metadata monitor")
-    monitor = MetadataMonitor()
-    all_checks = monitor.get_metadata_integrity_checks()
-    summaries = monitor.get_all_metadata_integrity_summaries()
-    des = monitor.get_data_elements_to_monitor()
-    orgunit = monitor.get_level1_orgunits()
-    period = time.strftime("%Y%m%d")
-    monitor.process_completed_checks_to_data_values(summaries, period, orgunit["organisationUnits"][0]["id"], des)
-    logging.info("Metadata monitor completed")
+    parser = argparse.ArgumentParser(description='Metadata Monitor')
+    parser.add_argument('--config', type=str, required=True, help='Path to the config.ini file')
+    args = parser.parse_args()
+    monitor = MetadataMonitor(args.config)
+    monitor.create_missing_data_elements()
+    monitor.update_metadata_integrity_data_elements()
